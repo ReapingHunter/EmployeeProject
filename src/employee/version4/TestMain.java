6f21368c21273d5de804fbc331f684705e3542a3
ReapingHunter <114232547+ReapingHunter@users.noreply.github.com>
2023-09-27 15:27:08 +0800

    Update TestMain.java

--- src/employee/version4/TestMain.java
+++ src/employee/version4/TestMain.java
@@ -6,7 +6,7 @@
 package employee.version4;
 import java.time.*;
 
-/**
+/**^
  *
  * @author User
  */
@@ -15,9 +15,11 @@ public class TestMain {
     public static void main(String[] args) {
         EmployeeRoster empList = new EmployeeRoster(20);
         HourlyEmployee h = new HourlyEmployee(20103264, new Name("Marlex", "Manalili", "Acorin"), LocalDate.of(2015, 5, 14), LocalDate.of(2003, 5, 13), 45.4f, 315);
+        HourlyEmployee h2 = new HourlyEmployee(1, new Name("The", "Keeper", "K"), LocalDate.of(2015, 5, 14), LocalDate.of(2003, 5, 13), 45.4f, 315);
+        empList.addEmployee(h2);
         empList.addEmployee(h);
         empList.displayEmployee(h);
-        System.out.println(empList.countPWE());
+        System.out.println(empList.countHE());
     }
 }
 
